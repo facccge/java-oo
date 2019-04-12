b94c0d987a6f15762b6f46887c22ef510889e2f0
facccge <fcfc5702@qq.com>
2019-04-12 09:19:18 +0800

    Completed Practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,25 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        if (number > 0) {
+            return "Class " + number;
+        }else{
+            return "No Class";
+        }
+    }
 }
